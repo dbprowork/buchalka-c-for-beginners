2d86f6506feab7319489f9e6189dc708a1896d51
Daniel Brown <121890999+dbprowork@users.noreply.github.com>
2024-02-23 16:10:48 +0000

    Create section7-video64.c

---
+++ section7-video64.c
@@ -0,0 +1,53 @@
+/*
+Generate random number from 0-20
+
+Get user guess
+
+Check if guess is valid
+
+Return "too high", "too low" or a win message if the user guesses the number within 5 tries
+
+Note: generating a random number:
+    include stdlib and time
+    use srand function
+    create a time variable: time_t t
+    initialise random number generator: srand((unsigned) time(&t))
+    generate number: int randomNumber = rand() % 21
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    int i = 5, guess;
+    time_t t;
+
+    srand((unsigned) time(&t));
+    int randomNumber = rand() % 21;
+
+    printf("Guess a number between 0 and 20.\n");
+
+    while (i > 0) {
+        printf("You have %d attempt(s).\n", i);
+        printf("Enter a guess: ");
+        scanf("%d", &guess);
+        if (guess == randomNumber) {
+            printf("Congratulations. You guessed the correct number!\n");
+            break;
+        }
+        else if (guess < randomNumber) {
+            printf("Too low.\n");
+        }
+        else if (guess > randomNumber) {
+            printf("Too high.\n");
+        }
+        i--;
+    }
+
+    if (i == 0) {
+        printf("You have no attempts left.\n");
+    }
+
+    return 0;
+}
