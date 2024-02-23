41a4fab122dacab7df0360c0de2cd2da9b4f85db
Daniel Brown <121890999+dbprowork@users.noreply.github.com>
2024-02-23 16:36:44 +0000

    Update section7-video64.c
    Add guess validation
    Add message differentiating 1 attempt from n attempts

--- section7-video64.c
+++ section7-video64.c
@@ -29,10 +29,20 @@ int main(void) {
     printf("Guess a number between 0 and 20.\n");
 
     while (i > 0) {
-        printf("You have %d attempt(s).\n", i);
+        if (i == 1){
+            printf("You have 1 attempt.\n");
+        }
+        else {
+            printf("You have %d attempts.\n", i);
+        }
+
         printf("Enter a guess: ");
         scanf("%d", &guess);
-        if (guess == randomNumber) {
+        
+        if (guess < 0 || guess > 20) {
+            printf("Number must be between 0 and 20.\n");
+        }
+        else if (guess == randomNumber) {
             printf("Congratulations. You guessed the correct number!\n");
             break;
         }
@@ -46,7 +56,7 @@ int main(void) {
     }
 
     if (i == 0) {
-        printf("You have no attempts left.\n");
+        printf("You have no attempts left.\nThe number was %d.\n", randomNumber);
     }
 
     return 0;
